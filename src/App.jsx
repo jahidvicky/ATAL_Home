82369d242456c515a23109cf733340737572f9c2
Dipanshu <dipanshu.patel.wwl@gmail.com>
2025-10-03 13:04:33 +0530

    merge and update local

--- src/App.jsx
+++ src/App.jsx
@@ -57,7 +57,6 @@ import UpdateProfile from "./page/updateProfile/UpdateProfile";
 import Collections from "./page/collections/Collections";
 import LensSelection from "./page/lensSelection/LensSelectionFlow";
 import TrackOrder from "./page/order/TrackOrder";
-import TestLoader from "./loader/TestLoader";
 
 function App() {
   const [currentUserId, setCurrentUserId] = useState(localStorage.getItem("user") || null);
@@ -160,10 +159,6 @@ function App() {
         {
           path: "/services",
           element: <Services />,
-        },
-        {
-          path: "/loading",
-          element: <TestLoader />,
         },
         {
           path: "/category",
--- src/page/Cart/Cartpage.jsx
+++ src/page/Cart/Cartpage.jsx
@@ -157,9 +157,8 @@ const Cartpage = () => {
                     <img
                       src={img}
                       alt={`frame-${index}`}
-                      className={`w-[100px] hover:cursor-pointer rounded ${
-                        mainImage === img ? "ring-2 ring-green-700" : ""
-                      }`}
+                      className={`w-[100px] hover:cursor-pointer rounded ${mainImage === img ? "ring-2 ring-green-700" : ""
+                        }`}
                     />
                   </button>
                 ))}
@@ -209,11 +208,10 @@ const Cartpage = () => {
                             <div
                               key={key}
                               onClick={() => toggleSize(letter)}
-                              className={`px-4 py-2 border rounded cursor-pointer text-center transition-all ${
-                                isSelected
+                              className={`px-4 py-2 border rounded cursor-pointer text-center transition-all ${isSelected
                                   ? "bg-red-500 text-white border-red-500"
                                   : "bg-white text-black border-gray-300 hover:border-red-500"
-                              }`}
+                                }`}
                             >
                               {letter}
                             </div>
@@ -241,11 +239,10 @@ const Cartpage = () => {
                           onClick={() => setSelectedColor(color.trim())} // trim extra spaces
                           style={{ backgroundColor: color.trim() }}
                           className={`w-6 h-6 rounded-full cursor-pointer transition-all
-            ${
-              selectedColor === color.trim()
-                ? "border-2 border-red-500"
-                : "border border-gray-300"
-            }
+            ${selectedColor === color.trim()
+                              ? "border-2 border-red-500"
+                              : "border border-gray-300"
+                            }
           `}
                         ></span>
                       ))}
@@ -291,7 +288,7 @@ const Cartpage = () => {
                     <p>
                       <strong>Prescription:</strong>{" "}
                       {lensDetails.lens.selectedLens ===
-                      "Non-prescription lenses"
+                        "Non-prescription lenses"
                         ? "Not required"
                         : lensDetails.lens.prescriptionMethod || "Not provided"}
                     </p>
@@ -358,11 +355,10 @@ const Cartpage = () => {
 
                   <Link to="lens-selection-flow" state={{ ID: ID }}>
                     <button
-                      className={`${
-                        isLensSelected
+                      className={`${isLensSelected
                           ? "bg-gray-400 cursor-not-allowed"
                           : "bg-black hover:bg-gray-900"
-                      } text-white px-42 py-3 mb-4 rounded ml-10 text-xl border-1 border-black w-115`}
+                        } text-white px-42 py-3 mb-4 rounded ml-10 text-xl border-1 border-black w-115`}
                       disabled={isLensSelected}
                     >
                       {isLensSelected ? "Lens Selected" : "SELECT LENS"}
