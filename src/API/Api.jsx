ac8b8aaceba26a6e2def8a8bfdbaa32e4a51bb71
jahidvicky <jahidvicky@gmail.com>
2026-04-08 11:11:28 +0530

    loomis and api fix

--- src/API/Api.jsx
+++ src/API/Api.jsx
@@ -7,16 +7,16 @@ export const registerLoader = (fn) => {
 };
 
 // local
-export const CHAT_API_URL = "http://localhost:4000/api";
-export const SOCKET_URL = "http://localhost:4000";
-export const IMAGE_URL = "http://localhost:4000/uploads/";
-export const PDF_URL = "http://localhost:4000/api";
+// export const CHAT_API_URL = "http://localhost:4000/api";
+// export const SOCKET_URL = "http://localhost:4000";
+// export const IMAGE_URL = "http://localhost:4000/uploads/";
+// export const PDF_URL = "http://localhost:4000/api";
 
 // LIVE
-// export const CHAT_API_URL = "https://api.ataloptical.org/api";
-// export const SOCKET_URL = "https://api.ataloptical.org";
-// export const IMAGE_URL = "https://api.ataloptical.org/uploads/";
-// export const PDF_URL = "https://api.ataloptical.org/api";
+export const CHAT_API_URL = "https://api.ataloptical.org/api";
+export const SOCKET_URL = "https://api.ataloptical.org";
+export const IMAGE_URL = "https://api.ataloptical.org/uploads/";
+export const PDF_URL = "https://api.ataloptical.org/api";
 
 const API = axios.create({
   baseURL: CHAT_API_URL,
